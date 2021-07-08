8b776527b57d4460111c3a5c072d85637b2badfa
damirbaisalov <baisalovdamir@gmail.com>
2021-07-08 12:29:16 +0600

    Registration drafts with splash screen

---
+++ app/src/main/java/kz/bfgroup/bikli/registration/Registration.kt
@@ -0,0 +1,13 @@
+package kz.bfgroup.bikli.registration
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kz.bfgroup.bikli.R
+
+class Registration: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registration)
+    }
+}
