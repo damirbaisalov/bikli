5ed899b56399d74ad4211c17e3de35e039094e0c
damirbaisalov <baisalovdamir@gmail.com>
2021-07-09 12:41:12 +0600

    finished with bottomNav but with DEPRECATED METHOD

--- app/src/main/java/kz/bfgroup/bikli/main_window/presentation/MainWindow.kt
+++ app/src/main/java/kz/bfgroup/bikli/main_window/presentation/MainWindow.kt
@@ -2,36 +2,82 @@ package kz.bfgroup.bikli.main_window.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.FrameLayout
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import kz.bfgroup.bikli.R
-import kz.bfgroup.bikli.databinding.ActivityMainBinding
+import kz.bfgroup.bikli.main_window.cart_fragment.presentation.CartFragment
+import kz.bfgroup.bikli.main_window.history_fragment.presentation.HistoryFragment
+import kz.bfgroup.bikli.main_window.home_fragment.presentation.HomeFragment
+import kz.bfgroup.bikli.main_window.promotion_fragment.presentation.PromotionFragment
+import kz.bfgroup.bikli.main_window.user_fragment.presentation.UserFragment
 
 class MainWindow : AppCompatActivity() {
 
-    lateinit var binding: ActivityMainBinding
-//    private lateinit var bottomNavigationView: BottomNavigationView
+    private lateinit var nMainFrame: FrameLayout
+    private lateinit var bottomNavigationView: BottomNavigationView
+
+    private lateinit var homeFragment: HomeFragment
+    private lateinit var promotionFragment: PromotionFragment
+    private lateinit var userFragment: UserFragment
+    private lateinit var historyFragment: HistoryFragment
+    private lateinit var cartFragment: CartFragment
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_main_window)
 
-//        initViews()
+        initViews()
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
+
+        loadFragments(homeFragment)
 
     }
-//
-//    private fun initViews() {
-//        bottomNavigationView = findViewById(R.id.main_window_bottom_navigation)
-//    }
 
-//    private fun loadFragments(fragment: Fragment) {
-//
-//        val transaction = supportFragmentManager.beginTransaction()
-//        transaction.replace(R.id.container, fragment)
-//        transaction.addToBackStack(null)
-//        transaction.commit()
-//    }
+    private fun initViews() {
+        nMainFrame = findViewById(R.id.main_window_frame_layout)
+        bottomNavigationView = findViewById(R.id.main_window_bottom_navigation)
 
+        homeFragment = HomeFragment()
+        promotionFragment = PromotionFragment()
+        userFragment = UserFragment()
+        historyFragment = HistoryFragment()
+        cartFragment = CartFragment()
+    }
 
+    private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener {
+        when(it.itemId){
+            R.id.item1 ->  {
+                loadFragments(homeFragment)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.item2 -> {
+                loadFragments(promotionFragment)
+                return@OnNavigationItemSelectedListener  true
+            }
+            R.id.item3 ->{
+                loadFragments(userFragment)
+                return@OnNavigationItemSelectedListener  true
+            }
+            R.id.item4 -> {
+                loadFragments(historyFragment)
+                return@OnNavigationItemSelectedListener  true
+            }
+            R.id.item5 ->{
+                loadFragments(cartFragment)
+                return@OnNavigationItemSelectedListener  true
+            }
+        }
+        false
+    }
+
+    private fun loadFragments(fragment: Fragment) {
+
+        val fragmentTransaction: FragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.main_window_frame_layout, fragment)
+        fragmentTransaction.commit()
+    }
 }
