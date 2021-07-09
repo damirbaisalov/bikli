60686cc1561f3a72c82d64eb21ad1d11a898c17c
damirbaisalov <baisalovdamir@gmail.com>
2021-07-09 15:45:45 +0600

    Added bikli helper for home,promotion,history

--- app/src/main/java/kz/bfgroup/bikli/main_window/promotion_fragment/presentation/PromotionFragment.kt
+++ app/src/main/java/kz/bfgroup/bikli/main_window/promotion_fragment/presentation/PromotionFragment.kt
@@ -4,10 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import androidx.fragment.app.Fragment
 import kz.bfgroup.bikli.R
 
 class PromotionFragment: Fragment() {
+
+    private lateinit var bikliHelperOn: LinearLayout
+    private lateinit var bikliHelperOff: LinearLayout
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -16,5 +20,34 @@ class PromotionFragment: Fragment() {
     ): View? {
 
         return inflater.inflate(R.layout.fragment_promotion, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initViews()
+        bikliHelperOn.visibility = View.GONE
+        bikliHelperOff.setOnClickListener {
+            checkVisibility()
+        }
+
+        bikliHelperOn.setOnClickListener {
+            checkVisibility()
+        }
+    }
+
+    private fun initViews() {
+        bikliHelperOn = view?.findViewById(R.id.fragment_promotion_bikli_helper_linear_layout)!!
+        bikliHelperOff = view?.findViewById(R.id.fragment_promotion_hand_help_linear_layout)!!
+    }
+
+    private fun checkVisibility() {
+        if (bikliHelperOff.visibility == View.VISIBLE && bikliHelperOn.visibility == View.GONE) {
+            bikliHelperOff.visibility = View.GONE
+            bikliHelperOn.visibility = View.VISIBLE
+        } else if (bikliHelperOff.visibility == View.GONE && bikliHelperOn.visibility == View.VISIBLE) {
+            bikliHelperOff.visibility = View.VISIBLE
+            bikliHelperOn.visibility = View.GONE
+        }
     }
 }
