9d65b27bb9950dd88f7e564da466d77119731beb
damirbaisalov <baisalovdamir@gmail.com>
2021-08-02 12:38:50 +0600

    CodeDialogFragment Added

--- app/src/main/java/kz/bfgroup/bikli/registration/presentation/Registration.kt
+++ app/src/main/java/kz/bfgroup/bikli/registration/presentation/Registration.kt
@@ -60,8 +60,10 @@ class Registration: AppCompatActivity() {
                 "floor" to flatFloorEditText.text.toString(),
                 "street" to flatNumberEditText.text.toString()
             )
-            registerUser()
+//            registerUser()
             Toast.makeText(this@Registration, fields.toString(), Toast.LENGTH_LONG).show()
+            val dialog = CodeDialogFragment()
+            dialog.show(supportFragmentManager, "codeDialog")
         }
     }
 
